4646c8d5bf8aa89e239b72737af7bcbe088d3317
Lin32587 <48487129+Lin32587@users.noreply.github.com>
2019-04-14 18:45:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ btnOK.onclick = function () {
     else {
         if (a + b > c && a + c > b && b + c > a) {
             if (a == b || b == c || a == c) {
-                if (a == b == c) {
+                if (a == b&&b == c&&a==c) {
                     alert("等边三角形");
                 }
                 else if(a*a==b*b+c*c||b*b==a*a+c*c||c*c==a*a+b*b){
